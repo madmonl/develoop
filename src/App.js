5fd14227cc22509fbb8ba5acc6461357c8ebac55
madmonl <madmonl@post.bgu.ac.il>
2019-12-02 16:43:12 +0200

    Home page design

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './styles/styles.scss';
 
 function App() {
   return (
-    <div className='app'>
-      <header className='app-header'>
-        <img src='/public/images/hero-image.jpg' alt='hero' height="53" width="53"></img>
-      </header>
+    <div className='home-container'>      
+      <div className='terminal'>
+        <div className='dots'>
+          <div className='dot dot__turquoise'></div>
+          <div className='dot dot__red'></div>
+          <div className='dot dot__green'></div>
+        </div>
+        <div className='text-box'>
+          <h1 className='header-primary'>
+            <p className='header-primary-main'>Develoop</p>
+            <p className='header-primary-sub'>Coding challenges platform</p>
+          </h1>
+          <a href='#' className='btn btn-white'>Create challenge</a>
+        </div>
+        <a href='#' className='btn-header'>Explore</a>
+      </div>
     </div>
   );
 }
